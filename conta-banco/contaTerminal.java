993911cf14bd2b1099bde1d3c3dea2b85444c72e
Rafael Sartori da Silva <126919029+RafaSartorios@users.noreply.github.com>
2023-08-27 17:08:58 -0300

    Codigo completo com tratamento de exceções incluso

---
+++ conta-banco/contaTerminal.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class contaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
+
+        while (continuar) {
+            try {
+                System.out.print("Por favor, digite o número da Agência: ");
+                int numeroAgencia = lerNumero(scanner);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+            }
+
+          
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
