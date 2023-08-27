62c465df2aaf9429cab32baa9685c77482164ad9
Rafael Sartori da Silva <126919029+RafaSartorios@users.noreply.github.com>
2023-08-27 17:11:06 -0300

    Completo com Exceções

--- conta-banco/contaTerminal.java
+++ conta-banco/contaTerminal.java
@@ -10,59 +10,59 @@ public class contaTerminal {
                 System.out.print("Por favor, digite o número da Agência: ");
                 int numeroAgencia = lerNumero(scanner);
 
+                System.out.print("Digite o número da Conta: ");
+                int numeroConta = lerNumero(Scanner);
 
+                System.out.print("Digite o nome do Cliente: ");
+                String nomeCliente = lerNomeCliente(Scanner);
 
+                System.out.print("Digite o saldo da Conta: ")
+                double saldoConta = lerNumeroDecimal(Scanner);
 
+                System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, ");
+                System.out.println("sua agência é " + numeroAgencia + ", conta " + numeroConta + "e seu saldo é R$" + saldoConta);
 
-
-
-
-
-
-
-
-
-
-
-
-
-                
+                System.out.println("\nDeseja cadastrar um novo cliente? (1 para sim, 2 para não): ");
+                int opcao = lerNumero(Scanner);
+                if (opcao == 2) {
+                    continuar = false;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida. Certifique-se de digitar os dados corretamente.");
             }
-
-          
         }
 
-
+        scanner.close();
     }
 
+    private static int lerNumero(Scanner scanner) {
+        while (true) {
+            try {
+                return integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.print("Número inválido. Por favor, insira um número: ");
+            }
+        }
+    }
 
+    private static double lerNumeroDecimal(Scanner scanner) {
+        while (true) {
+            try {
+                return Double.parseDouble(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.print("Número inválido. Por favor, insira um número: ");
+            }
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    private static String lerNomeCliente(Scanner scanner) {
+        while (true) {
+            String nome = scanner.nextLine();
+            if (nome.matches("[a-zA-Z]+")) {
+                return nome;
+            } else {
+                System.out.print("Nome inválido. Por favor, insira apenas letras: ")
+            }
+        }
+    }
 }
